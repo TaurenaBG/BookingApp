9edabad6e4dbd2dd6394ca0e5436c6318368db01
Miroslav Ivanov <mailfortesy@gmail.com>
2024-10-26 18:39:34 +0300

    added model Tour and a global class for constants and  error messages

---
+++ BookingApp/Data/Models/Tour.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static TourAgency.GlobalConstantsAndMessages;
+namespace TourAgency.Data.Models
+{
+    public class Tour
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(TourNameMaxLenght, ErrorMessage = TourNameMaxLenghtErrorMsg)]
+        [MinLength(TourNameMinLenght, ErrorMessage = TourNameMinLenghtErrorMsg)]
+        [Comment("Tour Name")]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [MaxLength(TourDescMaxLenght, ErrorMessage = TourDescMaxLenghtErrorMsg)]
+        [MinLength(TourDescMinLenght, ErrorMessage = TourDescMinLenghtErrorMsg)]
+        [Comment("Tour Description")]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        [Range(PriceMinAmount, PriceMaxAmount, ErrorMessage = PriceInvalidAmountMsg)]
+        [Comment("The price of the tour")]
+        public decimal Price { get; set; }
+
+
+        [Required]
+        [MaxLength(TourDestinationMaxLenght, ErrorMessage = TourDestinationMaxLenghtErrorMsg)]
+        [MinLength(TourDestinationMinLenght, ErrorMessage = TourDestinationMinLenghtErrorMsg)]
+        [Comment("Destination of the tour")]
+        public string Destionation { get; set; } = null!;
+
+        [Required(ErrorMessage = StartDateNullMsg)]
+        [DataType(DataType.Date)]
+        [Comment("Starting date")]
+        public DateTime StartDate { get; set; }
+
+        [Required(ErrorMessage = EndDateNullMsg)]
+        [DataType(DataType.Date)]
+        [Comment("End date")]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        [Comment("Shows if the tour is available")]
+        public bool IsAvailable { get; set; } = true;
+    }
+}
+
+
+
+
+
+
---
+++ BookingApp/GlobalConstantsAndMessages.cs
@@ -0,0 +1,38 @@
+﻿namespace TourAgency
+{
+    public static class GlobalConstantsAndMessages
+    {
+        // Tour Name
+        public const int TourNameMaxLenght = 60;
+        public const string TourNameMaxLenghtErrorMsg = "Tour name must be less than 60 characters";
+        public const int TourNameMinLenght = 3;
+        public const string TourNameMinLenghtErrorMsg = "Tour name must be more than 3 characters";
+
+        // Tour Description
+        public const int TourDescMaxLenght = 500;
+        public const string TourDescMaxLenghtErrorMsg = "Tour Description Must be less than 500 characters";
+        public const int TourDescMinLenght = 1;
+        public const string TourDescMinLenghtErrorMsg = "Tour Description is required";
+
+        // Tour Price
+        public const double PriceMinAmount = 1.00;
+        public const double PriceMaxAmount = 9999.00;
+        public const string PriceInvalidAmountMsg = "The price of the Tour should be between 1.00 and 9999.00";
+
+        // Tour Destination
+
+        public const int TourDestinationMaxLenght = 100;
+        public const string TourDestinationMaxLenghtErrorMsg = "Destination must be less than 100 characters";
+        public const int TourDestinationMinLenght = 3;
+        public const string TourDestinationMinLenghtErrorMsg = "Destination must be more than 3 characters";
+
+        // Tour StartDate 
+
+        public const string DateTimeFormat = "dd-MM-yyyy";
+        public const string StartDateNullMsg = "Start Date is required";
+        public const string EndDateNullMsg = "End Date is required";
+
+
+
+    }
+}
